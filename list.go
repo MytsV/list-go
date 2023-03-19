b2a3a188491e4e89ee0e0df026771d49ffadb99d
MytsV <mytsvictoria@gmail.com>
2023-03-19 20:48:46 +0200

    Implement methods, already covered by tests
    Use double linked list as implementation method as per variant

--- list.go
+++ list.go
@@ -1,13 +1,38 @@
 package list
 
+import "fmt"
+
+type Node struct {
+	prev *Node
+	next *Node
+	key  rune
+}
+
 type List struct {
+	head *Node
+	tail *Node
 }
 
 func (l *List) Length() int {
-	return -1
+	length := 0
+	node := l.head
+	for node != nil {
+		node = node.next
+		length++
+	}
+	return length
 }
 
 func (l *List) Append(element rune) {
+	node := &Node{key: element}
+	if l.head == nil {
+		l.head = node
+		l.tail = node
+	} else {
+		l.tail.next = node
+		node.prev = l.tail
+		l.tail = node
+	}
 }
 
 func (l *List) Insert(element rune, index int) {
@@ -20,7 +45,16 @@ func (l *List) DeleteAll(element rune) {
 }
 
 func (l *List) Get(index int) (rune, error) {
-	return '\x00', nil
+	if index < 0 || index >= l.Length() {
+		return '\x00', fmt.Errorf("Invalid index %d", index)
+	}
+	node := l.head
+	pos := 0
+	for pos != index {
+		pos++
+		node = node.next
+	}
+	return node.key, nil
 }
 
 func (l *List) Clone(index int) List {
