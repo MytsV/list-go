08a013a44aa9a91b40b54ff8431e8320e2d93908
MytsV <mytsvictoria@gmail.com>
2023-03-21 04:33:39 +0200

    Finish second implementation of list
    Make a deliberate bug to crash tests

--- list.go
+++ list.go
@@ -2,36 +2,16 @@ package list
 
 import "fmt"
 
-type Node struct {
-	prev *Node
-	next *Node
-	key  rune
-}
-
 type List struct {
-	head *Node
-	tail *Node
+	slice []rune
 }
 
 func (l *List) Length() int {
-	length := 0
-	node := l.head
-	for node != nil {
-		node = node.next
-		length++
-	}
-	return length
+	return len(l.slice)
 }
 
 func (l *List) Append(element rune) {
-	node := &Node{key: element}
-	if l.head == nil {
-		l.head = node
-	} else {
-		l.tail.next = node
-		node.prev = l.tail
-	}
-	l.tail = node
+	l.slice = append(l.slice, element)
 }
 
 func (l *List) Insert(element rune, index int) error {
@@ -43,18 +23,8 @@ func (l *List) Insert(element rune, index int) error {
 		return nil
 	}
 
-	inserted := &Node{key: element}
-	current := l.getNode(index)
-	inserted.next = current
-
-	if index == 0 {
-		l.head.prev = inserted
-		l.head = inserted
-	} else {
-		current.prev.next = inserted
-		inserted.prev = current.prev
-		current.prev = inserted
-	}
+	l.slice = append(l.slice[:index+1], l.slice[index:]...)
+	l.slice[index] = element
 	return nil
 }
 
@@ -63,35 +33,19 @@ func (l *List) Delete(index int) error {
 		return fmt.Errorf("Invalid index %d", index)
 	}
 
-	if index == 0 {
-		l.head = l.head.next
-		if l.head != nil {
-			l.head.prev = nil
-		}
-	} else if index == l.Length()-1 {
-		l.tail = l.tail.prev
-		l.tail.next = nil
-	} else {
-		deleted := l.getNode(index)
-		deleted.prev.next = deleted.next
-		deleted.next.prev = deleted.prev
-	}
+	l.slice = append(l.slice[:index], l.slice[index+1:]...)
 	return nil
 }
 
 func (l *List) DeleteAll(element rune) {
-	node := l.head
-	pos := 0
-	for pos < l.Length() {
-		if node.key == element {
-			node = node.next
+	for pos := 0; pos < len(l.slice); {
+		if l.slice[pos] == element {
 			err := l.Delete(pos)
 			if err != nil {
 				panic("Can't delete all elements because of internal error")
 			}
 		} else {
 			pos++
-			node = node.next
 		}
 	}
 }
@@ -100,82 +54,45 @@ func (l *List) Get(index int) (rune, error) {
 	if index < 0 || index >= l.Length() {
 		return '\x00', fmt.Errorf("Invalid index %d", index)
 	}
-	node := l.getNode(index)
-	return node.key, nil
+	return l.slice[index], nil
 }
 
 func (l *List) Clone() *List {
 	clone := &List{}
-	node := l.head
-	for node != nil {
-		clone.Append(node.key)
-		node = node.next
-	}
+	cloneSlice := make([]rune, len(l.slice))
+	copy(cloneSlice, l.slice)
+	clone.slice = cloneSlice
 	return clone
 }
 
 func (l *List) Reverse() {
-	var temp *Node
-	current := l.head
-
-	for current != nil {
-		temp = current.prev
-		current.prev = current.next
-		current.next = temp
-		current = current.prev
-	}
-
-	if temp != nil {
-		l.head = temp.prev
+	for i, j := 0, len(l.slice)-1; i < j; i, j = i+1, j-1 {
+		l.slice[i], l.slice[j] = l.slice[j], l.slice[i]
 	}
 }
 
 func (l *List) FindFirst(element rune) int {
-	node := l.head
-	pos := 0
-	for node != nil {
-		if node.key == element {
-			return pos
+	for pos := 0; pos < len(l.slice); pos++ {
+		if l.slice[pos] == element {
+			return -1
 		}
-		pos++
-		node = node.next
 	}
 	return -1
 }
 
 func (l *List) FindLast(element rune) int {
-	node := l.tail
-	pos := l.Length() - 1
-	for node != nil {
-		if node.key == element {
+	for pos := len(l.slice) - 1; pos >= 0; pos-- {
+		if l.slice[pos] == element {
 			return pos
 		}
-		pos--
-		node = node.prev
 	}
 	return -1
 }
 
 func (l *List) Clear() {
-	l.head = nil
-	l.tail = nil
+	l.slice = []rune{}
 }
 
 func (l *List) Extend(elements List) {
-	node := elements.head
-	for node != nil {
-		l.Append(node.key)
-		node = node.next
-	}
-}
-
-// "Private" method. Isn't included in the interface
-func (l *List) getNode(index int) *Node {
-	node := l.head
-	pos := 0
-	for pos != index {
-		pos++
-		node = node.next
-	}
-	return node
+	l.slice = append(l.slice, elements.slice...)
 }
