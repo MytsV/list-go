008bde8a2137d6a9d06004a68bbdd6b951683b25
MytsV <mytsvictoria@gmail.com>
2023-03-21 02:14:37 +0200

    Implement and test more methods
    Implement Insert, Delete and DeleteAll

--- list.go
+++ list.go
@@ -36,26 +36,68 @@ func (l *List) Append(element rune) {
 }
 
 func (l *List) Insert(element rune, index int) error {
+	if index < 0 || index > l.Length() {
+		return fmt.Errorf("Invalid index %d", index)
+	}
+	if index == l.Length() {
+		l.Append(element)
+		return nil
+	}
+
+	newNode := &Node{key: element}
+	if index == 0 {
+		newNode.next = l.head
+		l.head.prev = newNode
+		l.head = newNode
+	} else {
+		node := l.getNode(index)
+		newNode.next = node
+		node.prev.next = newNode
+		newNode.prev = node.prev
+		node.prev = newNode
+	}
 	return nil
 }
 
 func (l *List) Delete(index int) error {
+	if index < 0 || index >= l.Length() {
+		return fmt.Errorf("Invalid index %d", index)
+	}
+	if index == 0 {
+		l.head = l.head.next
+		if l.head != nil {
+			l.head.prev = nil
+		}
+	} else if index == l.Length()-1 {
+		l.tail = l.tail.prev
+		l.tail.next = nil
+	} else {
+		toDelete := l.getNode(index)
+		toDelete.prev.next = toDelete.next
+		toDelete.next.prev = toDelete.prev
+	}
 	return nil
 }
 
 func (l *List) DeleteAll(element rune) {
+	node := l.head
+	pos := 0
+	for pos < l.Length() {
+		if node.key == element {
+			node = node.next
+			l.Delete(pos)
+		} else {
+			pos++
+			node = node.next
+		}
+	}
 }
 
 func (l *List) Get(index int) (rune, error) {
 	if index < 0 || index >= l.Length() {
 		return '\x00', fmt.Errorf("Invalid index %d", index)
 	}
-	node := l.head
-	pos := 0
-	for pos != index {
-		pos++
-		node = node.next
-	}
+	node := l.getNode(index)
 	return node.key, nil
 }
 
@@ -78,4 +120,14 @@ func (l *List) Clear() {
 }
 
 func (l *List) Extend(elements List) {
+}
+
+func (l *List) getNode(index int) *Node {
+	node := l.head
+	pos := 0
+	for pos != index {
+		pos++
+		node = node.next
+	}
+	return node
 }
--- list_test.go
+++ list_test.go
@@ -20,6 +20,27 @@ func getList(length int) List {
 	return list
 }
 
+func constructList(slice []rune) List {
+	list := List{}
+	for i := 0; i < len(slice); i++ {
+		list.Append(slice[i])
+	}
+	return list
+}
+
+func areSame(l List, slice []rune) bool {
+	if l.Length() != len(slice) {
+		return false
+	}
+	for i := 0; i < len(slice); i++ {
+		value, err := l.Get(i)
+		if value != slice[i] || err != nil {
+			return false
+		}
+	}
+	return true
+}
+
 func TestLength(t *testing.T) {
 	type testCase struct {
 		msg    string
@@ -103,9 +124,220 @@ func TestGet(t *testing.T) {
 		if test.hasErr {
 			assert.NotNil(err, test.msg)
 		} else {
+			assert.Nil(err, test.msg)
 			assert.Equal(getStubElement(test.idx), value, test.msg)
 		}
 		assert.Equal(length, list.Length(),
 			"list.Get() has no side effects")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	length := 20
+
+	type testCase struct {
+		msg    string
+		idx    int
+		hasErr bool
+	}
+
+	testCases := []testCase{
+		{
+			msg:    "Correctly inserts an element at the head",
+			idx:    0,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly inserts an element at the end",
+			idx:    length - 1,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly inserts an element at the middle",
+			idx:    length / 2,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly inserts an element at random position",
+			idx:    length / 4,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly appends an element",
+			idx:    length,
+			hasErr: false,
+		},
+		{
+			msg:    "Throws an error when index is negative",
+			idx:    -1,
+			hasErr: true,
+		},
+		{
+			msg:    "Throws an error when index is out of bonds",
+			idx:    length + 1,
+			hasErr: true,
+		},
+	}
+
+	assert := assert.New(t)
+	testElement := '1'
+	for _, test := range testCases {
+		list := getList(length)
+		err := list.Insert(testElement, test.idx)
+
+		if test.hasErr {
+			assert.NotNil(err, test.msg)
+			continue
+		}
+
+		//No error is produced, list length changed
+		assert.Nil(err, test.msg)
+		assert.Equal(length+1, list.Length(), test.msg)
+
+		//New element is at the insert position
+		value, err := list.Get(test.idx)
+		assert.Nil(err, test.msg)
+		assert.Equal(testElement, value, test.msg)
+
+		//Previous element stayed in place
+		if test.idx != 0 {
+			previous, err := list.Get(test.idx - 1)
+			assert.Nil(err, test.msg)
+			assert.Equal(getStubElement(test.idx-1), previous, test.msg)
+		}
+
+		//Initial element changed position
+		if test.idx != length {
+			next, err := list.Get(test.idx + 1)
+			assert.Nil(err, test.msg)
+			assert.Equal(getStubElement(test.idx), next, test.msg)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	length := 5
+
+	type testCase struct {
+		msg    string
+		idx    int
+		hasErr bool
+	}
+
+	testCases := []testCase{
+		{
+			msg:    "Correctly deletes first element",
+			idx:    0,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly deletes last element",
+			idx:    length - 1,
+			hasErr: false,
+		},
+		{
+			msg:    "Correctly deletes an element in the middle",
+			idx:    length / 2,
+			hasErr: false,
+		},
+		{
+			msg:    "Throws an error when index is negative",
+			idx:    -1,
+			hasErr: true,
+		},
+		{
+			msg:    "Throws an error when index is out of bonds",
+			idx:    length,
+			hasErr: true,
+		},
+	}
+
+	assert := assert.New(t)
+	for _, test := range testCases {
+		list := getList(length)
+		err := list.Delete(test.idx)
+
+		if test.hasErr {
+			assert.NotNil(err, test.msg)
+			continue
+		}
+
+		//No error produced, length changed
+		assert.Nil(err, test.msg)
+		assert.Equal(length-1, list.Length(), test.msg)
+
+		//Deleted index is now occupied with next value
+		if test.idx != length-1 {
+			value, err := list.Get(test.idx)
+			assert.Nil(err, test.msg)
+			assert.Equal(getStubElement(test.idx+1), value, test.msg)
+		}
+
+		//Previous element's position is intact
+		if test.idx != 0 {
+			value, err := list.Get(test.idx - 1)
+			assert.Nil(err, test.msg)
+			assert.Equal(getStubElement(test.idx-1), value, test.msg)
+		}
+	}
+}
+
+func TestDeleteAll(t *testing.T) {
+	type testCase struct {
+		msg     string
+		input   []rune
+		output  []rune
+		element rune
+	}
+
+	testCases := []testCase{
+		{
+			msg:     "Correctly deletes one middle element",
+			input:   []rune{'a', 'b', 'c', 'd'},
+			output:  []rune{'a', 'b', 'd'},
+			element: 'c',
+		},
+		{
+			msg:     "Correctly deletes one first element",
+			input:   []rune{'a', 'b', 'c', 'd'},
+			output:  []rune{'b', 'c', 'd'},
+			element: 'a',
+		},
+		{
+			msg:     "Correctly deletes one last element",
+			input:   []rune{'a', 'b', 'c', 'd'},
+			output:  []rune{'a', 'b', 'c'},
+			element: 'd',
+		},
+		{
+			msg:     "Correctly deletes multiple elements : 1",
+			input:   []rune{'a', 'b', 'b', 'a', 'a', 'b', 'a'},
+			output:  []rune{'b', 'b', 'b'},
+			element: 'a',
+		},
+		{
+			msg:     "Correctly deletes multiple elements : 2",
+			input:   []rune{'b', 'a', 'b', 'a', 'b'},
+			output:  []rune{'b', 'b', 'b'},
+			element: 'a',
+		},
+		{
+			msg:     "Correctly deletes all elements",
+			input:   []rune{'a', 'a', 'a', 'a', 'a'},
+			output:  []rune{},
+			element: 'a',
+		},
+		{
+			msg:     "Deletes no elements if those are not present",
+			input:   []rune{'a', 'a', 'a', 'a', 'a'},
+			output:  []rune{'a', 'a', 'a', 'a', 'a'},
+			element: 'b',
+		},
+	}
+
+	for _, test := range testCases {
+		list := constructList(test.input)
+		list.DeleteAll(test.element)
+		assert.True(t, areSame(list, test.output), test.msg)
 	}
 }
